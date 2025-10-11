0807108819e1252b4e2d9fc1560f30ce78a6a909
nurealazmie002 <nurealazmie002@gmail.com>
2025-10-11 16:45:25 +0700

    final

--- week2/day4/tugas/src/components/DataManipulation.jsx
+++ week2/day4/tugas/src/components/DataManipulation.jsx
@@ -7,7 +7,6 @@ function FilteredUserList() {
     fetch("https://jsonplaceholder.typicode.com/users")
       .then((res) => res.json())
       .then((data) => {
-        // contoh: ambil hanya 5 user pertama
         const sliced = data.slice(0, 5);
         setUsers(sliced);
       });
@@ -15,7 +14,7 @@ function FilteredUserList() {
 
   return (
     <div className="p-4 bg-white shadow rounded-md text-center">
-      <h2 className="font-bold text-blue-600 mb-2">Daftar User (Top 5)</h2>
+      <h2 className="font-bold text-blue-600 mb-2">Daftar User</h2>
       <ul>
         {users.map((u) => (
           <li key={u.id} className="border p-2 m-1 rounded text-gray-600">
